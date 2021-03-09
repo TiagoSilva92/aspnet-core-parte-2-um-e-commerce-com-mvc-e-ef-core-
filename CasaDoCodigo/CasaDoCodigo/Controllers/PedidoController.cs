49427b5b67a20bda52c7848fa660faa42c6fecb0
tiagosilvabpr <tiagodasilva360@hotmail.com>
2021-03-09 07:35:32 -0300

    Chamando repositório de itens a partir do controller

--- CasaDoCodigo/CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/CasaDoCodigo/Controllers/PedidoController.cs
@@ -12,11 +12,15 @@ namespace CasaDoCodigo.Controllers
     {
         private readonly IProdutoRepository produtoRepository;
         private readonly IPedidoRepository pedidoRepository;
+        private readonly IItemPedidoRepository itemPedidoRepository;
         
-        public PedidoController(IProdutoRepository produtoRepository, IPedidoRepository pedidoRepository)
+        public PedidoController(IProdutoRepository produtoRepository, 
+            IPedidoRepository pedidoRepository,
+            IItemPedidoRepository itemPedidoRepository)
         {
             this.produtoRepository = produtoRepository;
             this.pedidoRepository = pedidoRepository;
+            this.itemPedidoRepository = itemPedidoRepository;
         }
        
         public IActionResult Carrossel()
@@ -47,7 +51,7 @@ namespace CasaDoCodigo.Controllers
         [HttpPost]
         public void UpdateQuantidade([FromBody]ItemPedido itemPedido)
         {
-
+            itemPedidoRepository.UpdateQuantidade(itemPedido);
         }
     }
 }
